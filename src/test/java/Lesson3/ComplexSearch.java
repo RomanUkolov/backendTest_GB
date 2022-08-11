695cacfe2bc83b357921d3d5cde6e6c873f88c42
Roman Ukolov <Roman_tora@mail.ru>
2022-08-11 13:54:50 +0300

    tests for get requests

---
+++ src/test/java/Lesson3/ComplexSearch.java
@@ -0,0 +1,81 @@
+package Lesson3;
+
+import io.restassured.path.json.JsonPath;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+public class ComplexSearch extends AbstractTest {
+
+    @Test
+    void onlyAuth() {
+        given()
+                .queryParam("apiKey", getApiKey())
+                .when()
+                .get(getBaseUrl() + "recipes/complexSearch")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .header("Content-Type", "application/json");
+    }
+
+    @Test
+    void cuisinesValidValue() {
+        JsonPath responce = given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("cuisine", "Irish")
+                .when()
+                .get(getBaseUrl() + "recipes/complexSearch")
+                .body()
+                .jsonPath();
+
+        assertThat(responce.get("results[1].title"), equalTo("Corned Beef and Cabbage"));
+    }
+
+    @Test
+    void minProteine() {
+        given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("minProtein", "10")
+                .when()
+                .get(getBaseUrl() + "recipes/complexSearch")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .header("Content-Type", "application/json")
+                .body(containsString("Protein"));
+
+    }
+
+    @Test
+    void showRecipeNutrition() {
+        given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("minProtein", "10")
+                .when()
+                .get(getBaseUrl() + "recipes/complexSearch")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .header("Content-Type", "application/json")
+                .body(containsString("nutrition"));
+
+    }
+    @Test
+    void showIngridients() {
+        given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("minProtein", "10")
+                .when()
+                .get(getBaseUrl() + "recipes/complexSearch")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .header("Content-Type", "application/json")
+                .body(containsString("usedIngredientCount"))
+                .body(containsString("missedIngredientCount"))
+                .body(containsString("missedIngredients"));
+    }
+}
