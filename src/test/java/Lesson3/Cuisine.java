5688055ac6a10f1e72eb5a0af79fb53f6de3801d
Roman Ukolov <Roman_tora@mail.ru>
2022-08-11 14:19:32 +0300

    create class

--- src/test/java/Lesson3/Cuisine.java
+++ src/test/java/Lesson3/Cuisine.java
@@ -1,4 +1,25 @@
 package Lesson3;
 
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
 public class Cuisine extends AbstractTest{
+
+    @Test
+    void emptyReqest() {
+        given()
+                .queryParam("apiKey", getApiKey())
+                .contentType("application/x-www-form-urlencoded")
+                .when()
+                .post(getBaseUrl() + "recipes/cuisine")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .header("Content-Type", "application/json")
+                .body(containsString("cuisine"));
+    }
+
 }
