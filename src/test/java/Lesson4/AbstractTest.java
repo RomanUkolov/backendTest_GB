932bdcfe7e68bc2661831cdc9cc6be466a86a9fa
Roman Ukolov <Roman_tora@mail.ru>
2022-08-15 10:19:22 +0300

    add jackson in pom. add new data in var.prop. create class AbstactTest

---
+++ src/test/java/Lesson4/AbstractTest.java
@@ -0,0 +1,67 @@
+package Lesson4;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class AbstractTest {
+
+    static Properties prop = new Properties();
+    private static InputStream configFile;
+    private static String apiKey;
+    private static String baseUrl;
+    private static String hash;
+    protected static ResponseSpecification responseSpecification;
+    protected static RequestSpecification requestSpecification;
+
+    @BeforeAll
+    static void initTest() throws IOException {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        configFile = new FileInputStream("src/main/resources/var.properties");
+        prop.load(configFile);
+
+        apiKey =  prop.getProperty("apiKey");
+        baseUrl = prop.getProperty("base_url");
+        hash = prop.getProperty("hash");
+
+        responseSpecification = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectStatusLine("HTTP/1.1 200 OK")
+                .expectContentType(ContentType.JSON)
+                .build();
+
+        requestSpecification = new RequestSpecBuilder()
+                .addQueryParam("apiKey", apiKey)
+                .addQueryParam("hash",hash )
+                .setContentType(ContentType.JSON)
+                .build();
+
+        RestAssured.responseSpecification = responseSpecification;
+        RestAssured.requestSpecification = requestSpecification
+    }
+
+    public static String getApiKey() {
+        return apiKey;
+    }
+
+    public static String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public static String getHash() {
+        return hash;
+    }
+
+    public RequestSpecification getRequestSpecification(){
+        return requestSpecification;
+    }
+}
